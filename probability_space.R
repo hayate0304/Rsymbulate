439be260c1f1e87f07b35523397732aa214e76e8
Kien Nguyen <luckien20006@gmail.com>
2018-08-10 19:46:49 -0700

    Almost done ProbabilitySpace

--- probability_space.R
+++ probability_space.R
@@ -1,3 +1,7 @@
+setwd("C:/Users/lucki/Desktop/Frost Research/Rsymbulate")
+source("seed.R")
+source("sequences.R")
+
 ProbabilitySpace <- function(drawFunc){
   me <- list(
     draw = drawFunc
@@ -8,44 +12,187 @@ ProbabilitySpace <- function(drawFunc){
   return(me)
 }
 
-# draw <- function(elObjeto, newValue = 1){
-#   print("Calling the base function")
-#   UseMethod("draw")
-# }
-# 
-# draw.default <- function(object, newValue){
-#   return(object)
-# }
-# 
-# draw.ProbabilitySpace <- function(elObjeto, newValue = 1){
-#   print("In draw.Prob and setting the value")
-#   elObjeto$draw <- newValue
-#   return(elObjeto)
-# }
-
-sim <- function(self, value){
-  UseMethod("sim")
+draw <- function(self){
+  UseMethod("draw")
 }
 
-sim.default <- function(self, value){
+draw.default <- function(self){
+  print("In default")
   return(NULL)
 }
 
+draw.ProbabilitySpace <- function(self){
+#  print("In PS")
+  return(self$draw())
+}
+
+sim <- function(self, n){
+  UseMethod("sim")
+}
+
+sim.default <- function(self, n)
+  return(NULL)
+
+
 sim.ProbabilitySpace <- function(self, n){
   vect <- numeric(length(n))
-  for (i in 1:n) vect[i] <- ProbabilitySpace$draw
+  for (i in 1:n) vect[i] <- draw(self)
   return(vect)
 }
 
-ArbitrarySpace <- function(drawFunc){
-  me <- ProbabilitySpace(drawFunc)
-  me$draw = function() 1
-    
+check_same <- function(self, other){
+  UseMethod("check_same")
+}
+
+check_same.default <- function(self, other)
+  return(NULL)
+
+
+check_same.ProbabilitySpace <- function(self, other){
+  # if (inherits(other, "ArbitrarySpace")){
+  #   return()
+  # } else 
+  if (!identical(self, other))
+    stop("Events must be defined on same probability space.")
+}
+
+`%*%` <- function(self, other){
+  UseMethod("%*%")
+}
+
+`%*%.default` <- function(self, other){
+  return(NULL)
+}
+
+`%*%.ProbabilitySpace` <- function(self, other){
+  draw <- function(self, other){
+    a = draw(self)
+    b = draw(other)
+    return(c(a, b))
+  }
+  return(ProbabilitySpace(drawFunc = draw))
+#  return(c(draw(self), draw(other)))
+}
+
+#--------------------------------------------------
+### Define inheritance classes
+ArbitrarySpace <- function(){
+  me <- list(
+    draw = function() 1
+  )
+
   # Add name for the class
-  class(me) <- append(class(me), "ArbitrarySpace")
+  class(me) <- list.append(class(me), "ProbabilitySpace", "ArbitrarySpace")
   return(me)
 }
 
-BoxModel <- function(){
+#--------------------------------------------------------
+BoxModel <- function(box, size = 1, replace = TRUE, 
+                     probs = NULL, order_matters = TRUE){
+
+# size = NULL just to have similar behavior with Python
+# but default is 1
+  
+  if (is.atomic(box)){
+    self.box = box
+
+  } else if (is.list(box)){
+      self.box = unlist(lapply(
+        seq_along(box), function(x) rep(names(box)[x], box[[x]])))
+  } else 
+      stop("Box must be specified either as a vector or a list.")
+  
+  me <- list(box = self.box,
+             size = size,
+             replace = replace,
+             probs = probs,
+             order_matters = order_matters)
+             
+  ## Add name for the class
+  class(me) <- list.append(class(me), "BoxModel", "ProbabilitySpace")
+  return(me)
+}
+
+# draw <- function(self){
+#   UseMethod("draw")
+# }
+# 
+# draw.default <- function(self){
+#   return(NULL)
+# }
+
+draw.BoxModel <- function(self){
+  print("In BoxModel")
+  
+  draw_inds <- function(size){
+    return(sample(length(self$box), size, self$replace, self$prob)) 
+  }
+  
+  if (self$size == 1){
+    return(self$box[draw_inds(1)])
+  } else if (is.infinite(self$size)) {
+#    warning("BoxModel with size = Inf will be implemented later")
+    
+    if (self$replace == FALSE)
+      stop("Cannot draw an infinite number of tickets
+           without replacement")
+
+    seed <- get_seed()
+    
+    x <- function(n){
+      set.seed(seed)
+      replicate(as.integer(n), draw_inds(1))
+
+      return(self$box[draw_inds(1)])
+    }
+
+    return(getitem(InfiniteSequence(x), n))
+  } else {
+    draws = self$box[draw_inds(self$size)]
+    if (!self$order_matters)
+      draws = sort(draws)
+    
+    return(draws)
+  }
   
 }
+
+DeckOfCards <- function(size = 1, replace = FALSE, order_matters = TRUE){
+  deck <- paste(rep(c(2:10, "J", "Q", "K", "A"), 4),  #card values
+                rep(c("Diamonds", "Hearts", "Clubs", "Spades"), each = 13)) #suits
+  
+  me <- list(box = deck,
+             size = size,
+             replace = replace,
+             probs = NULL,
+             order_matters = order_matters)
+  
+  class(me) <- list.append(class(me), "ProbabilitySpace", "BoxModel", "DeckOfCards")
+  return(me)
+}
+
+Event <- function(probSpace, fun){
+  me <- list(probSpace = probSpace,
+             fun = fun)
+  
+  class(me) <- append(class(me), "Event")
+  return(me)
+}
+
+check_same_probSpace <- function(self, other){
+  UseMethod("check_same_probSpace")
+}
+
+check_same_probSpace.default <- function(self, other){
+  return(NULL)
+}
+
+check_same_probSpace.Event <- function(self, other){
+  check_same(self$probSpace, other$probSpace)
+}
+
+
+
+
+
+
