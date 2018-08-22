14604953327856f969a9e8d2efab1d5d857b9433
Kien Nguyen <luckien20006@gmail.com>
2018-08-22 13:33:58 -0700

    delete some files

--- seed.R
+++
@@ -1,9 +0,0 @@
-
-seed = as.integer(Sys.time())
-
-get_seed <- function(){
-  seed <<- seed + 1
-  
-  if (seed > .Machine$integer.max) seed = 0
-  return(seed)
-}
--- utils.R
+++
@@ -1,8 +0,0 @@
-
-is_scalar <- function(x) is.numeric(x) && length(x) == 1L 
-
-is_vector <- function(x) is.numeric(x) && identical(dim(x), NULL)
-
-get_dimesion <- function(x){
-  return(length(dim(x$results)))
-}
