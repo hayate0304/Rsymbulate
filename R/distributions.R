03fcd916c4abe377e63ac8400f020ca37b58359d
Kien Nguyen <luckien20006@gmail.com>
2018-09-11 18:47:19 -0700

    Add distributions

--- R/distributions.R
+++ R/distributions.R
@@ -1,5 +1,6 @@
 
 #' @import ggplot2
+#' @import extraDistr
 
 wrap <- function(fun, args){
   p <- paste0("do.call(", fun, args, ")")
@@ -39,6 +40,9 @@ plot.Distribution <- function(self, type = NULL, alpha = 0.4,
     xvals <- seq(xlower, xupper, length.out = 100)
 
   yvals <- self$pdf(xvals)
+  print(xvals)
+  if (inherits(self, "NegativeBinomial"))
+    yvals <- self$pdf(xvals - self$params[[1]])
 
   color <- get_next_color()
   col <- color()
@@ -173,6 +177,102 @@ Hypergeometric <- function(n, N0, N1){
 draw.Hypergeometric <- function(self)
   return(rhyper(1, self$N1, self$N0, self$n))
 
+#' Defines a probability space for a geometric
+#' distribution (which represents the number
+#' of trials until the first success), including
+#' the success.
+#'
+#' @param p (double): probability (number between 0 and 1)
+#' that each trial results in a "success" (i.e., 1)
+#' @export
+Geometric <- function(p){
+  if (!(p > 0 && p < 1))
+    stop("p must be between 0 and 1")
+
+  params = list(p)
+
+  attribute <- Distribution(params, "geom", TRUE)
+  attribute$xlim = c(1, attribute$xlim[2])  # Geometric distributions are not defined for x < 1
+  attribute[["p"]]= p
+
+  class(attribute) <- append("Geometric", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Geometric distribution.
+#' @export
+draw.Geometric <- function(self)
+  # rgeom exclude the success, so we need to add 1
+  return(1 + rgeom(1, self$p))
+
+#' Defines a probability space for a negative
+#' binomial distribution (which represents the
+#' number of trials until r successes), including
+#' the r successes.
+#'
+#' @param r (int): desired number of successes
+#' @param p (double): probability (number between 0 and 1)
+#' that each trial results in a "success" (i.e., 1)
+#' @export
+NegativeBinomial <- function(r, p){
+  if (!(r > 0 && is.numeric(r) && round(r) == r))
+    stop("r must be a positive integer")
+
+  if (!(p > 0 && p <= 1))
+    stop("p must be between 0 and 1")
+
+  params = list(r, p)
+
+  attribute <- Distribution(params, "nbinom", TRUE)
+  attribute[["p"]]= p
+  attribute[["r"]]= r
+  attribute$xlim = c(r, attribute$xlim[2])  # Negative Binomial distributions are not defined for x < 0
+
+  class(attribute) <- append("NegativeBinomial", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Negative Binomial distribution.
+#' @export
+draw.NegativeBinomial <- function(self)
+  return(self$r + rnbinom(1, self$r, self$p))
+
+#' Defines a probability space for a Pascal
+#' distribution (which represents the number
+#' of trials until r successes), not including
+#' the r successes.
+#'
+#' @param r (int): desired number of successes
+#' @param p (double): probability (number between 0 and 1)
+#' that each trial results in a "success" (i.e., 1)
+#' @export
+Pascal <- function(r, p){
+  if (!(r > 0 && is.numeric(r) && round(r) == r))
+    stop("r must be a positive integer")
+
+  if (!(p > 0 && p <= 1))
+    stop("p must be between 0 and 1")
+
+  params = list(r, p)
+
+  attribute <- Distribution(params, "nbinom", TRUE)
+  attribute$xlim = c(0, attribute$xlim[2])  # Pascal distributions are not defined for x < r
+  attribute[["p"]]= p
+  attribute[["r"]]= r
+
+  class(attribute) <- append("Pascal", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Negative Binomial distribution.
+#' @export
+draw.Pascal <- function(self)
+  return(rnbinom(1, self$r, self$p))
+
+
 #' Defines a probability space for a Poisson distribution.
 #'
 #' @param lam (float): rate parameter for the Poisson distribution
@@ -209,7 +309,14 @@ DiscreteUniform <- function(a = 0, b = 1){
   attribute <- list(a = a,
              b = b)
 
-  class(attribute) <- c(class(attribute), "DiscreteUniform", "Distribution", "ProbabilitySpace")
+  params <- list(a, b)
+
+  attribute <- Distribution(params, "dunif", TRUE)
+  attribute$xlim = c(a, b)
+  attribute[["a"]]= a
+  attribute[["b"]]= b
+
+  class(attribute) <- append("DiscreteUniform", class(attribute))
   return(attribute)
 }
 
@@ -313,6 +420,7 @@ Exponential <- function(rate = 1.0, scale = NULL){
   attribute <- Distribution(params, "exp", FALSE)
   attribute$xlim = c(0, attribute$xlim[2])
   attribute[["rate"]]= rate
+  attribute[["scale"]]= scale
 
   class(attribute) <- append("Exponential", class(attribute))
   return(attribute)
@@ -327,3 +435,206 @@ draw.Exponential <- function(self){
   } else
     return(rexp(1, 1 / self$scale))
 }
+
+#' Defines a probability space for a gamma distribution.
+#' Only one of scale or rate should be set. (The scale is the
+#'                                           inverse of the rate.)
+#'
+#' @param shape (double): shape parameter for gamma distribution
+#' (often symbolized alpha)
+#' @param rate (double): rate parameter for gamma distribution
+#' (often symbolized lambda)
+#' @param scale (double): scale parameter for gamma distribution
+#' (often symbolized beta = 1 / lambda)
+#' @export
+Gamma <- function(shape, rate = 1.0, scale = NULL){
+  if (shape < 0)
+    stop("shape parameter must be positive")
+  if (identical(scale, NULL)){
+    if (rate < 0)
+      stop("rate must be positive")
+  } else
+    if (scale < 0)
+      stop("scale must be positive")
+
+
+  rate <- ifelse(identical(scale, NULL), rate, 1 / scale)
+  params <- list(shape, rate)
+
+  attribute <- Distribution(params, "gamma", FALSE)
+  attribute$xlim = c(0, attribute$xlim[2]) # Gamma distributions are not defined for x < 0
+  attribute[["rate"]]= rate
+  attribute[["scale"]]= scale
+  attribute[["shape"]]= shape
+
+  class(attribute) <- append("Gamma", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Gamma distribution.
+#' @export
+draw.Gamma <- function(self)
+  if (identical(self$scale, NULL)){
+    return(rgamma(1, self$shape, self$rate))
+  } else
+    return(rgamma(1, self$shape, scale = self$scale))
+
+#' Defines a probability space for a beta distribution.
+#'
+#' @param a (double): alpha parameter for beta distribution
+#' @param b (double): beta parameter for beta distribution
+#' @export
+Beta <- function(a=1, b=1, xmin=0, xmax=1, scale=NULL){
+  if (a < 0)
+    stop("a must be positive")
+  if (b < 0)
+    stop("b must be positive")
+  if (xmin > xmax)
+    stop("xmax cannot be less than xmin")
+
+  params = list(a, b)
+
+  attribute <- Distribution(params, "beta", FALSE)
+  attribute$xlim = c(0, 1) # Beta distributions are not defined for x < 0 and x > 1
+  attribute[["a"]]= a
+  attribute[["b"]]= b
+  attribute[["xmin"]]= xmin
+  attribute[["xmax"]]= xmax
+
+  class(attribute) <- append("Beta", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Beta distribution.
+#' @export
+draw.Beta <- function(self)
+  return(self$xmin + (self$xmax - self$xmin) * rbeta(1, self$a, self$b))
+
+#' Defines a probability space for Student's t distribution.
+#'
+#' @param df (int): degrees of freedom
+#' @export
+StudentT <- function(df){
+  if (df < 0)
+    stop("df must be greater than 0")
+
+  params <- list(df)
+
+  attribute <- Distribution(params, "t", FALSE)
+  if (df == 1){
+    attribute$mean <- function() NaN
+    attribute$sd <- function() NaN
+    attribute$var <- function() NaN
+  }
+  attribute[["df"]]= df
+
+  class(attribute) <- append("StudentT", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the T distribution.
+#' @export
+draw.StudentT <- function(self)
+  return(rt(1, self$df))
+
+#' Defines a probability space for chi-square distribution.
+#'
+#' @param df (int): degrees of freedom
+#' @export
+ChiSquare <- function(df){
+  if (!(df > 0 && is.numeric(df) && round(df) == df))
+    stop("df must be a positive integer")
+
+  params <- list(df)
+
+  attribute <- Distribution(params, "chisq", FALSE)
+  attribute$xlim <- c(0, attribute$xlim[2]) # Chi-Square distributions are not defined for x < 0
+  attribute[["df"]]= df
+
+  class(attribute) <- append("ChiSquare", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the ChiSquare distribution.
+#' @export
+draw.ChiSquare <- function(self)
+  return(rchisq(1, self$df))
+
+#' Defines a probability space for F distribution.
+#'
+#' @param dfN (int): degrees of freedom in the numerator
+#' @param dfD (int): degrees of freedom in the denominator
+#' @export
+F <- function(dfN, dfD){
+  if (dfN < 0)
+    stop("dfN must be greater than 0")
+
+  if (dfD < 0)
+    stop("dfD must be greater than 0")
+
+  params <- list(dfN, dfD)
+
+  attribute <- Distribution(params, "f", FALSE)
+  attribute$xlim <- c(0, attribute$xlim[2]) # F distributions are not defined for x < 0
+  attribute[["dfN"]]= dfN
+  attribute[["dfD"]]= dfD
+
+  class(attribute) <- append("F", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the F distribution.
+#' @export
+draw.F <- function(self)
+  return(rf(1, self$dfN, self$dfD))
+
+#' Defines a probability space for Cauchy distribution.
+#'
+#' The Cauchy distribution has no parameters
+#' @export
+Cauchy <- function(loc = 0, scale = 1){
+  params <- list(loc, scale)
+
+  attribute <- Distribution(params, "cauchy", FALSE)
+  attribute[["loc"]]= loc
+  attribute[["scale"]]= scale
+
+  class(attribute) <- append("Cauchy", class(attribute))
+  return(attribute)
+}
+
+#' A function that takes no arguments and
+#' returns a single draw from the Cauchy distribution.
+#' @export
+draw.Cauchy <- function(self)
+  return(self$loc + (self$scale * rcauchy(1, self$loc, self$scale)))
+
+#' Defines a probability space for a Log-Normal distribution
+#' If Y has a LogNormal distribution with parameters mu and sigma, then
+#' log(Y) has a normal distribution with mean mu and sd sigma.
+#'
+#' @param mu (double): mean of the underlying normal distribution
+#' @param sigma (double): standard deviation of the underlying normal distribution
+#' @export
+LogNormal <- function(mu = 0.0, sigma = 1.0){
+  if (sigma < 0)
+    stop("sigma must be greater than 0")
+
+  params <- list(mu, sigma)
+
+  attribute <- Distribution(params, "lnorm", FALSE)
+  attribute[["meanlog"]]= mu
+  attribute[["sdlog"]]= sigma
+
+  class(attribute) <- append("LogNormal", class(attribute))
+  return(attribute)
+}
+
+#' @export
+draw.LogNormal <- function(self)
+  return(rlnorm(1, self$meanlog, self$sdlog))
